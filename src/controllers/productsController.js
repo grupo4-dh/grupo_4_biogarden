390bca464517ae434322ae04a9662a4dcab43970
Melisa <melicabagnero@hotmail.com>
2020-11-26 12:45:19 -0300

    modifico media queries en detalle de producto!

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,9 +2,9 @@ const fs = require('fs')
 const path = require('path')
 
 // Usamos archivos JSON como base de datos momentáneamente
-// Los leemos (fs.readFileSync) y parseamos (JSON.parse) en una sola linea
-//let products = JSON.parse(fs.readFileSync(path.join(__dirname,'../database/products.json'),'utf8'))
-//let cart = JSON.parse(fs.readFileSync(path.join(__dirname,'../database/cart.json'),'utf8'))
+ //Los leemos (fs.readFileSync) y parseamos (JSON.parse) en una sola linea
+let products = JSON.parse(fs.readFileSync(path.join(__dirname,'../database/products.json'),'utf8'))
+let cart = JSON.parse(fs.readFileSync(path.join(__dirname,'../database/cart.json'),'utf8'))
 
 module.exports = {
     all: function(req, res) {
