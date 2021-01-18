130f87dc835c51ff1e89aeaa6f4462862bc500ba
Melisa <melicabagnero@hotmail.com>
2021-01-18 17:04:57 -0300

    se realizan los metodos de productos

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -25,22 +25,38 @@ module.exports = {
             }
         })
         .then(function(products) {
-            return res.render('./products/productsList', { products: products }); //recibe la ruta y el array
+            return res.render('productsList', { products: products }); //recibe la ruta y el array
         })
         .catch((error) => {
             return res.send(error)
         });
     },
+
+    search:function(req,resp){//faltaria hacer la vista de search resoult
+        db.Producto.findAll({
+            where:{
+                title:{
+                    [db.Sequelize.Op.like]:`%${req.query.search}%`
+                }
+            }
+        })
+        .then(function(resultado){
+            res.render('searchResoult',{
+                queryString:req.query.search,
+                productos:resultado
+            })
+        })
+
+    },
     // Devuelve la vista de Detalle de producto segun el id 
     detail: function(req, res) {
         db.Producto.findByPk(req.params.id,{
-            include:{
-                all:true,
-                nested:true
-            }
+            include:[{association:"colores"},{association:"tamanos"},{association:"categorias"},{association:"ordenes"}]
+                
+  
         })
         .then(function(product) {
-            return res.render('./products/productDetail', { product: product })
+            return res.render('productDetail', { product: product })
         })
         .catch((error) => {
             return res.send(error)
@@ -48,7 +64,7 @@ module.exports = {
     },
     // Devuelve la vista del Formulario de creación de producto 
     create: function(req, res) {
-        return res.render('./products/productCreate')
+        return res.render('productCreate')
     },
     // Guarda el producto que viaja en el body en la BBDD 
     save: function(req, res) {
@@ -72,41 +88,50 @@ module.exports = {
         for (product of productsCart) {
             total += product.price
         }
-        return res.render('./products/productsCart', { products: productsCart, total: total } )
+        return res.render('productsCart', { products: productsCart, total: total } )
     },
     
-    // Devuelve la vista de Edición de producto 
+    // Devuelve la vista de Edición de producto segun el ID
     edit: function(req, res) {
-        for (product of products) {
-            if (product.id == req.params.product_id) {
-                return res.render('./products/productEdit', { products: product })
-            }         
-        }
-        return res.send('PRODUCT NOT FOUND') // REDIGIR A LISTADO DE PRODUCTOS CON PARTIAL NOT FOUND
+        db.Producto.findByPK(req.prams.id,{
+            include:[{association:"colores"},{association:"tamanos"},{association:"categorias"},{association:"ordenes"}]
+        })
+        .then(function(elProducto){
+            res.render('productEdit',{elProducto:elProducto})
+        })
+
+       
     },
     // Actualiza un producto en la BBDD
     update: function(req, res) {
-        for (product of products) {
-            if (product.id == req.params.product_id) {
-                product.name = req.body.titulo,
-                product.description = req.body.descripcion,
-                product.image = req.file.filename,
-                product.category = req.body.categoria,
-                product.price = parseInt(req.body.precio),
-                product.colors = req.body.color 
+        db.Producto.update({
+            title:req.body.title,
+            price:req.body.price,
+            image:req.body.image,
+            description:req.body.descripcion,
+            id_category:req.body.id_category,
+            id_colour:req.body.id_colour,
+            id_size:req.body.id_size,
+            quantity:req.body.quantity
+
+        },{
+            where:{
+                id:req.params.id
             }
-        }
+        });
+        
+        res.redirect('productsList' + req.params.id)
 
-        //Escribimos el producto nuevo
-        fs.writeFileSync(path.join(__dirname,'../database/products.json'),JSON.stringify(products, null, 4));
-
-        // Redirigimos a la siguiente página home
-        res.redirect('/products')
+      
     },
-    // Borra un producto de la BBDD
+    // eliminar  un producto de la BBDD
     delete: function(req, res) {
-        products = products.filter( prod => prod.id != req.params.product_id )
-        fs.writeFileSync(path.join(__dirname,'../database/products.json'),JSON.stringify(products, null, 4));
-        res.redirect('/products')
+        db.Producto.destroy({
+            where:{
+                id:req.params.id
+            }
+        })
+        res.redirect('productList');
+        
     }
 }
--- src/routes/products.js
+++ src/routes/products.js
@@ -22,21 +22,24 @@ var upload = multer({ storage: storage })
 
 //  --- RUTAS --- 
 //  Listado de productos
-router.get('/', productsController.all)                     // Vista del Listado de productos
+router.get('/', productsController.all);                    // Vista del Listado de productos
+
+//Busqueda de productos desde el header
+router.get('/search',productsController.search);
 
 //  Creación de productos
-router.get('/create', productsController.create)            // Formulario de creación de productos
-router.post('/create', upload.single('imagen'), productsController.save)              // Acción de creación de producto
+router.get('/create', productsController.create);            // Formulario de creación de productos
+router.post('/create', upload.single('imagen'), productsController.save);              // Acción de creación de producto
 
-//  Carrito de compras
-router.get('/cart', productsController.cart)                // Vista del Carrito de compras
+//  Carrito de compras-----SOLO RENDERIZA LA VISTA , HAY QUE MEJORARLA
+router.get('/cart', productsController.cart);               // Vista del Carrito de compras
 
 //  Detalle de producto
-router.get('/:id', productsController.detail)       // Vista del Detalle de producto
+router.get('/:id', productsController.detail);       // Vista del Detalle de producto
 
 //  Edición y borrado de producto
-router.get('/:id/edit', productsController.edit)    // Vista del Formulario de edición de productos
-router.put('/:id', upload.single('producto'), productsController.update)        // Acción de edición de producto
-router.delete('/:id', productsController.delete)    // Acción de borrado de producto
+router.get('/:id/edit', productsController.edit);    // Vista del Formulario de edición de productos
+router.post('/:id/edit', upload.single('producto'), productsController.update);        // Acción de edición de producto
+router.post('/:id/delete', productsController.delete);    // Acción de borrado de producto
 
 module.exports = router; 
