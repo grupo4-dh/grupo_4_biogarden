ed4b0c83fa5c7e37959bc86149dce8ed14ef6601
Melisa <melicabagnero@hotmail.com>
2021-02-03 23:31:19 -0300

    modificacion de la validacion del front

--- public/jsfront/validacionUsuario.js
+++ public/jsfront/validacionUsuario.js
@@ -1,20 +1,25 @@
 
 window.addEventListener('load', function(){
 
-    let errores =[];
-    let formulario= document.querySelector("form#form");
-    let nombre= document.querySelector('input#name');
-    let lastName=document.querySelector('input#last_name');
-    let email =document.querySelector('input#email');
-    let pw=document.querySelector('input#pwd');
-    let repw =document.querySelector('input#repwd');
-    let avatar=document.querySelector('input#avatar')
+  
+    let formulario= document.querySelector("form.formulario");
+    let nombre= document.getElementById('name');
+    let lastName=document.getElementById('last_name');
+    let email =document.getElementById('email');
+    let pw=document.getElementById('pwd');
+    let repw =document.getElementById('repwd');
+    //let avatar=document.getElementById('avatar')
 
     formulario.addEventListener('submit',function(error){
-        error.preventDefault();
 
-        console.log(nombre);
-        if(nombre.value == " "){
+        
+        
+        let errores =[];
+
+        let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+        console.log(nombre.value);
+        if(nombre.value === " "){
             errores.push('El campo Nombre tiene que estar completo');
         
             }else if (nombre.value.length<2){
@@ -22,14 +27,30 @@ window.addEventListener('load', function(){
             }
             if(lastName.value == " "){
                 errores.push('El campo apellido tiene que estar completo');
-                
+
             }else if (lastname.value.length<2){
             errores.push('El campo apellido tiene que tener al menos 2 caracteres');
         }
+        if (!mailformat.test(email.value)){
+            errores.push('El email no es valido');
+        }
 
-        let ulErrores = document.querySelector("errores")
+        if(pw.value.length<8){
+            errores.push('Contrasena deberia tener minimo 6 caracteres');
+        }
+        if(repw.value===pw.value){
+            errores.push('las contrasenas deben coincidir');
+        }
+
+
+        if (errores.length>0){
+            error.preventDefault();
+
+        }
+        let ulErrores = document.querySelector("div.errores ul")
 
         for (let i=0; i<errores.length; i++){
+
             ulErrores.innerHTML+='<li>' + errores[i] + '</li>'
         }
 
